d32f3346be8f7ce7c5ac21a22d5c35dcfb74619e
Handsome36233 <1959317120@qq.com>
2024-05-12 00:42:10 +0800

    .

--- demo.py
+++ demo.py
@@ -22,15 +22,15 @@ className2label = {"Text": 0, "Image": 1, "PageIndicator": 2, "UpperTaskBar": 3,
 label2className = dict(zip(className2label.values(), className2label.keys()))
 image = cv2.imread("./test_image/108.jpg")
 H, W, _ = image.shape
-boxes, classes, scores = detect(image, detect_session, anchors, size=size)
-for box, c in zip(boxes, classes):
+boxes, labels, scores = detect(image, detect_session, anchors, size=size)
+for box, l in zip(boxes, labels):
     xmin, ymin, xmax, ymax = padding_box(box)
     xmin = int(xmin * W / size[0])
     ymin = int(ymin * H / size[1])
     xmax = int(xmax * W / size[0])
     ymax = int(ymax * H / size[1])
     cv2.rectangle(image, (xmin, ymin), (xmax, ymax), (0, 255, 0), 2)
-    # cv2.putText(image, label2className[c], (xmin, ymin - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 2)
+    # cv2.putText(image, label2className[l], (xmin, ymin - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 2)
 
 image = cv2.resize(image, (W // 2, H // 2))
 cv2.imshow('Object Detection', image)
